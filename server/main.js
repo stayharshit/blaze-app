6656f0641059edfde1b5145eea93c60f7baafcde
Harshit <harshit242016@gmail.com>
2025-12-17 09:59:32 +0530

    feat: Implement task management features with form and styling updates

--- server/main.js
+++ server/main.js
@@ -1,5 +1,23 @@
 import { Meteor } from 'meteor/meteor';
+import { TasksCollection } from '/imports/api/TasksCollection';
 
-Meteor.startup(() => {
-  // code to run on server at startup
+const SEED_TASKS = [
+  'First Task',
+  'Second Task',
+  'Third Task',
+  'Fourth Task',
+  'Fifth Task',
+  'Sixth Task',
+  'Seventh Task',
+];
+
+Meteor.startup(async () => {
+  const cursor = TasksCollection.find();
+  const existingCount = await cursor.countAsync();
+
+  if (existingCount === 0) {
+    const insertTask = (taskText) => TasksCollection.insertAsync({ text: taskText });
+
+    await Promise.all(SEED_TASKS.map(insertTask));
+  }
 });
